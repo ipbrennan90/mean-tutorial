be77074b4e922948fbaef487080b9969e145ac33
ipbrennan <ian.brennan@icloud.com>
2017-01-06 15:54:24 -0700

    basic upvoting and linking functionality added

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ app.controller('MainCtrl', [
 	function($scope){
 		$scope.test= 'Hello World';
 		$scope.posts = [
-			'post 1',
-			'post 2',
-			'post 3',
-			'post 4',
-			'post 5',
-			'post 6',
-		]
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2},
+			{title: 'post 3', upvotes: 15},
+			{title: 'post 4', upvotes: 9},
+			{title: 'post 5', upvotes: 4},
+			{title: 'post 6', upvotes: 3},
+		];
+
+		$scope.addPost =  function() {
+			if(!$scope.title || $scope.title === '') { return; }
+			$scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
+			$scope.title = '';
+			$scope.link = '';
+		};
+
+		$scope.upvote = function(post) {
+			post.upvotes += 1;
+		};
+
+
 	}
 ])
